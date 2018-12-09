e9c088d80480244d5aa635c448ad5e7b546e4d87
Tibbetts, Lucille Rose <ltibbetts@hk223-27734.local>
2018-12-09 00:49:32 -0800

    Gave up on getting tile map to work and switched to sprite nodes for the walls

--- ZagMan/GameScene.swift
+++ ZagMan/GameScene.swift
@@ -19,91 +19,73 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     enum NodeCategory: UInt32 {
         case spike = 1
         case basketball = 2
+        case wall = 4
     }
     
     // didMove is like viewDidLoad
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
         
-        // Load maze from GameScene.sks
-        maze = self.childNode(withName: "maze") as! SKTileMapNode
-        
         Spike = SKSpriteNode(imageNamed: "Spike")
-        Spike.size = CGSize(width: 55, height: 45)
-        Spike.position = CGPoint(x: 30, y: 110)
+        Spike.size = CGSize(width: 90, height: 80)
+        Spike.position = CGPoint(x: self.frame.midX, y: 80)
         Spike.physicsBody = SKPhysicsBody(circleOfRadius: Spike.size.height / 2)
         Spike.physicsBody?.affectedByGravity = false
         Spike.physicsBody?.categoryBitMask = NodeCategory.spike.rawValue
-        Spike.physicsBody?.contactTestBitMask = NodeCategory.basketball.rawValue
-        Spike.physicsBody?.collisionBitMask = 0
+        Spike.physicsBody?.contactTestBitMask = NodeCategory.basketball.rawValue | NodeCategory.wall.rawValue
         addChild(Spike)
         
         scoreLabel = SKLabelNode(text: "Basketballs: 0")
         scoreLabel.fontName = "System"
         scoreLabel.fontSize = 50
         scoreLabel.fontColor = UIColor.white
-        print(self.frame.minY)
         scoreLabel.position = CGPoint(x: self.frame.midX, y: self.frame.minY + 50)
         addChild(scoreLabel)
         
-        addBasketballs()
+        for node in children {
+            if node.name == "Wall" {
+                let sprite = node as! SKSpriteNode
+                sprite.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: sprite.size.width, height: sprite.size.height))
+                sprite.physicsBody?.affectedByGravity = false
+                sprite.physicsBody?.categoryBitMask = NodeCategory.wall.rawValue
+                sprite.physicsBody?.contactTestBitMask = NodeCategory.spike.rawValue
+                sprite.physicsBody?.collisionBitMask = 0
+                node.removeFromParent()
+                addChild(sprite)
+            } else if node.name == "Ball" {
+                let sprite = node as! SKSpriteNode
+                sprite.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.height / 2)
+                sprite.physicsBody?.affectedByGravity = false
+                sprite.physicsBody?.categoryBitMask = NodeCategory.basketball.rawValue
+                sprite.physicsBody?.contactTestBitMask = NodeCategory.spike.rawValue
+                sprite.physicsBody?.collisionBitMask = 0
+                node.removeFromParent()
+                addChild(sprite)
+            }
+        }
 
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
         if contact.bodyA.categoryBitMask == NodeCategory.basketball.rawValue {
             contact.bodyA.node?.removeFromParent()
+            score += 1
+            scoreLabel.text = "Basketballs: \(score)"
         } else if contact.bodyB.categoryBitMask == NodeCategory.basketball.rawValue {
             contact.bodyB.node?.removeFromParent()
-        }
-        score += 1
-        scoreLabel.text = "Basketballs: \(score)"
-        
-        if self.children.count == 3 {
-            // All basketballs retrieved (only Spike, maze, and label nodes are left)
-            print("You won!")
-        }
-    }
-    
-    func addBasketballs() {
-        var y = 470
-        while y > -300 {
-            var x = -270
-            while x < 280 {
-                let position = CGPoint(x: x, y: y)
-                let column = maze.tileColumnIndex(fromPosition: position)
-                let row = maze.tileRowIndex(fromPosition: position) - 2
-                let tile = maze.tileDefinition(atColumn: column, row: row)
-                if tile == nil {
-                    let ball = SKSpriteNode(imageNamed: "basketball")
-                    ball.size = CGSize(width: 30, height: 30)
-                    ball.position = position
-                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.height / 2)
-                    ball.physicsBody?.affectedByGravity = false
-                    ball.physicsBody?.categoryBitMask = NodeCategory.basketball.rawValue
-                    ball.physicsBody?.contactTestBitMask = NodeCategory.spike.rawValue
-                    ball.physicsBody?.collisionBitMask = 0
-                    addChild(ball)
-                }
-                x += 180
-            }
-            y -= 120
+            score += 1
+            scoreLabel.text = "Basketballs: \(score)"
+        } else if contact.bodyA.categoryBitMask == NodeCategory.wall.rawValue {
+            contact.bodyB.node?.removeAllActions() // Stop Spike
+        } else if contact.bodyB.categoryBitMask == NodeCategory.wall.rawValue {
+            contact.bodyA.node?.removeAllActions()
         }
     }
     
     func moveLocation(xMove: CGFloat, yMove: CGFloat) {
-        let currentX = Spike.position.x
-        let currentY = Spike.position.y
-        let newPosition = CGPoint(x: currentX + xMove*60, y: currentY + yMove*60)
-        let column = maze.tileColumnIndex(fromPosition: newPosition)
-        let row = maze.tileRowIndex(fromPosition: newPosition) - 2 // For some reason the row count needs to be adjusted by 2
-        let tile = maze.tileDefinition(atColumn: column, row: row)
-        if tile == nil {
-            let move = SKAction.moveBy(x: xMove*60, y: yMove*60, duration: 0.3) // Chose 60 to match tile size
-            Spike.run(move) {
-                // Closure that executes after Spike stops moving
-                self.moveLocation(xMove: xMove, yMove: yMove)
-            }
+        let move = SKAction.moveBy(x: xMove*60, y: yMove*60, duration: 0.25)
+        Spike.run(move) {
+            self.moveLocation(xMove: xMove, yMove: yMove)
         }
     }
     
